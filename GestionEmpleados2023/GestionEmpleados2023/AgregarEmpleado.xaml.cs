98faca0e3f4af494997482368bb6d376223e654f
imanoldam1B <iperezadam1B@gmail.com>
2023-12-10 23:21:05 +0100

    Primer commit

---
+++ GestionEmpleados2023/GestionEmpleados2023/AgregarEmpleado.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestionEmpleados2023
+{
+    /// <summary>
+    /// Lógica de interacción para AgregarEmpleado.xaml
+    /// </summary>
+    public partial class AgregarEmpleado : Window
+    {
+        public AgregarEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void AgregarEmpleado_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellidos = txtApellidos.Text;
+            bool esUsuario = chkEsUsuario.IsChecked ?? false;
+            int edad;
+
+            if (int.TryParse(txtEdad.Text, out edad) )
+            {
+                agregarEmpleadoString(nombre, apellidos, esUsuario, edad);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese una edad válida.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void agregarEmpleadoString(string nombre, string apellidos, bool esUsuario, int edad)
+        {
+            using (SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["GestionEmpleados2023.Properties.Settings.GestionEmpleadosConnectionString"].ConnectionString))
+            {
+                string consulta = "INSERT INTO EMPLEADOS (Nombre, Apellidos, EsUsuario, Edad) VALUES (@Nombre, @Apellidos, @EsUsuario, @Edad)";
+
+                using (SqlCommand cmd = new SqlCommand(consulta, conexion))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", nombre);
+                    cmd.Parameters.AddWithValue("@Apellidos", apellidos);
+                    cmd.Parameters.AddWithValue("@EsUsuario", esUsuario);
+                    cmd.Parameters.AddWithValue("@Edad", edad);
+
+                    try
+                    {
+                        conexion.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al agregar empleado: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ GestionEmpleados2023/GestionEmpleados2023/ListaEmpleados.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GestionEmpleados2023
+{
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public bool EsUsuario { get; set; }
+        public int Edad { get; set; }
+
+    }
+    public partial class GestionEmpleados
+    {
+        private SqlConnection conexionConSql;
+        public GestionEmpleados()
+        {
+            establecerConexion();
+        }
+        private void establecerConexion()
+        {
+            string cadenaDeConexion = ConfigurationManager.ConnectionStrings["GestionEmpleados2023.Properties.Settings.GestionEmpleadosConnectionString"].ConnectionString;
+            conexionConSql = new SqlConnection(cadenaDeConexion);
+        }
+        public List<Empleado> ObtenerEmpleados()
+        {
+            establecerConexion();
+
+            string consulta = "SELECT * FROM EMPLEADOS";
+            DataTable Empleados = new DataTable();
+
+            List<Empleado> listaEmpleados = new List<Empleado>();
+
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexionConSql);
+
+            using (adaptador)
+            {
+                adaptador.Fill(Empleados);
+            }
+            listaEmpleados = Empleados.AsEnumerable().Select(row => new Empleado
+            {
+                Nombre = row.Field<string>("Nombre"),
+                Apellidos = row.Field<string>("Apellidos"),
+                EsUsuario = (row["EsUsuario"] != DBNull.Value) ? row.Field<bool>("EsUsuario") : false,
+                Edad = row.Field<int>("Edad")
+            }).ToList();
+
+            return listaEmpleados;
+        }
+    }
+    public partial class ListaEmpleados : Window
+    {
+        private GestionEmpleados gestionEmpleados;
+        public ListaEmpleados()
+        {
+            InitializeComponent();
+            gestionEmpleados = new GestionEmpleados();
+            cargarEmpleadosEnDataGrid();
+        }
+        private void cargarEmpleadosEnDataGrid()
+        {
+            List<Empleado> empleados = gestionEmpleados.ObtenerEmpleados();
+            dataGrid.ItemsSource = empleados;
+        }
+    }
+        
+}
+
